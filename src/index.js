7f5867f77c8015476746506be017824c68240dd5
KmiloLopez <108908318+KmiloLopez@users.noreply.github.com>
2023-04-01 17:04:27 -0500

    j

--- src/index.js
+++ src/index.js
@@ -8,36 +8,26 @@ const galleryImage = doc.querySelector('.gallery')
 const loadMoreBtn = doc.querySelector('.load-more')
 const API_KEY = '34614174-fcdfb58168b28ad843b7da5c7';
 loadMoreBtn.hidden=true; 
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa",loadMoreBtn)
+
 let pageNumber=1;
 let perPage =40;
 let newUrl=("")
-///falta agregar axios, buscador deplegue 4 imagenes en fila, centrar buscador
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
-//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN//TODO BIEN
+
 
 const searchText = document.querySelector('[name="searchQuery"]');
 function encodeURIComponent(text){
-    console.log("Dentro de la funcion",text)
+    
     const encodedText = text.trim().split(" ").join("+")
-    console.log(encodedText)
+    
     return encodedText
 }
 searchForm.addEventListener('submit', (e) =>{
     e.preventDefault()
     galleryImage.innerHTML = null;
     const enteredText = searchText.value
-    console.log(enteredText)
+    
     const URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(enteredText)+"&image_type=photo&orientation=horizontal&safesearch=true"+`&per_page=${perPage}`+`&page=${pageNumber}`;
-    console.log("este es el URL: ", URL)
+    
     newUrl ="https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(enteredText)+"&image_type=photo&orientation=horizontal&safesearch=true"+`&per_page=${perPage}`;
     fetchImage(URL);
    
@@ -68,7 +58,7 @@ const fetchImages = async(theUrl)=>{
 function printImageCard(arrayImages){//aca llega el array de lo que encontro la busqueda
     //aca ponemos el if si no encontro nada manda alerta
     const totalphotos = arrayImages.totalHits
-    console.log("totalPhotos Encontramos esta cantidad de fotos con la busqueda",totalphotos)
+    
     arrayImages=arrayImages.hits
     if(totalphotos===0){
       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
@@ -115,10 +105,10 @@ function printImageCard(arrayImages){//aca llega el array de lo que encontro la 
 
     }
     if(pageNumber>1){
-      galleryImage.insertAdjacentHTML('beforeend',neededInfo) 
+      galleryImage.insertAdjacentHTML('beforeend',neededInfo) //este es el array que mapioo arrayImages
     }
-    console.log("este es el array que mapioo arrayImages:",arrayImages)
-    /* console.log("esto es lo que se esta intentando inyectar con innerHTM neededInfo:", neededInfo) */
+    
+    
     if(totalphotos>40){
       loadMoreBtn.hidden=false;
       const totalPages =parseInt(totalphotos/40)+1
